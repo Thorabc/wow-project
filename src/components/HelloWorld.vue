096b849d20f7ab68a748d10d93d2f992d2245bba
Thorabc <43777499+Thorabc@users.noreply.github.com>
2020-01-10 15:12:05 +0100

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,24 +8,24 @@
         <img src="https://render-us.worldofwarcraft.com/character/moon-guard/89/141749593-avatar.jpg" style="width:250px; margin-top:30px;" />
         
       </div>
-    </div> -->
-  
-    <div v-if="response">
-      <!-- <p>{{response.data.name}}</p> -->
-    </div>
+    </div>-->
     <div>
-      <input  type="text" v-model="userInputServer">
-      <input type="text" v-model="userInputName">
-      <button @click="getCharacter(userInputServer, userInputName), getThumbnail(userInputServer, userInputName)">Search</button>
+      <input type="text" v-model="userInputServer" />
+      <input type="text" v-model="userInputName" />
+      <button @click="search(userInputServer, userInputName)">Search</button>
       <button @click="getChar()">debug</button>
     </div>
     <div>
       <div v-for="character in characters" v-bind:key="character.id">
-        <p>{{character.name}}</p>
-        <p>{{character.realm.name}}</p>
-        <img src="http://render-{region}.worldofwarcraft.com/character/{character.thumbnail}">
+        <p>Name: {{character.name}}</p>
+        <p>Class: {{character.character_class.name}}</p>
+        <p>Realm: {{character.realm.name}}</p>
+        <p>Faction: {{character.faction.name}}</p>
+        <p>Spec: {{character.active_spec.name}}</p>
+        <img
+          v-bind:src="`http://render-${region}.worldofwarcraft.com/character/${character.avatar_url}`"
+        />
       </div>
-      
     </div>
   </div>
 </template>
@@ -35,7 +35,6 @@ import axios from "axios";
 //import { response } from 'express';
 
 export default {
-  
   // mounted: async function() {
   //   try {
   //     let response = await axios.get(
@@ -49,55 +48,63 @@ export default {
   data() {
     return {
       log: "no text",
-      response: {},
       error: "",
       userInputServer: "",
       userInputName: "",
       characters: [],
-      thumbnails: []
+      thumbnails: [],
+      region: "us"
     };
   },
   methods: {
+    async search(server, name){
+      this.getCharacter(server, name); 
+      this.getThumbnail(server, name);
+    },
     async getCharacter(server, name) {
       try {
-      let response = await axios.get(
-        "https://us.api.blizzard.com/profile/wow/character/" + server + "/" + name + "?namespace=profile-us&locale=en_US&access_token=US2B2jTNzSC4j3lVY5G91ha9Np667792nL",
+        let response = await axios.get(
+          "https://us.api.blizzard.com/profile/wow/character/" +
+            server +
+            "/" +
+            name +
+            "?namespace=profile-us&locale=en_US&access_token=US2B2jTNzSC4j3lVY5G91ha9Np667792nL"
         );
 
-      /* if (typeof.server == String) {
+        /* if (typeof.server == String) {
       } */
-      this.response = response;
-      this.characters.push(this.response.data)
-      
-    } catch (error) {
-      this.error = error;
-    }
-    },
-    
-     async getThumbnail(userInputServer, userInputName) {
-      try {
-      let response = await axios.get(
-        "https://us.api.blizzard.com/profile/wow/character/" + userInputServer + "/" + userInputName + "/character-media?namespace=profile-us&locale=en_US&access_token=USm5eeka21GQfFtZb4cvXaxYhJ4389yXll"
-        );
-     
 
-      //  if (typeof.server == String) {
-      // } 
-      this.response = response;
-      this.thumbnails.push(this.response.data)
-      console.log(this.thumbnails)
-      
-    } catch (error) {
-      this.error = error;
-    }
+        this.characters.push(response.data);
+        console.log(this.characters);
+      } catch (error) {
+        this.error = error;
+      }
     },
-    
+
+    async getThumbnail(server, name) {
+      try {
+        console.log("thumbnails");
+        let response = await axios.get(
+          "https://us.api.blizzard.com/profile/wow/character/" + server + "/" + 
+          name + "/character-media?namespace=profile-us&locale=en_US&access_token=US2B2jTNzSC4j3lVY5G91ha9Np667792nL"
+        );
+
+        //  if (typeof.server == String) {
+        // }
+        this.thumbnails.push(response.data);
+        console.log(this.thumbnails);
+      } catch (error) {
+        this.error = error;
+      }
+    },
+
     getChar: function() {
-      /* eslint-disable no-console */ 
+      /* eslint-disable no-console */
+
       /*console.log(this.response.data);*/
-      console.log(this.response.data)
+      console.log(this.response.data);
     }
-    },
+  }
 };
 </script>
 
